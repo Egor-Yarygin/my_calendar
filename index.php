304aebad29f2c70ebe305ef8d1c0b15122b23b1b
Egor-Yarygin <108565142+Egor-Yarygin@users.noreply.github.com>
2023-05-24 17:49:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Менеджер задач</title>
+</head>
+
+<body>
+    <div class="main">
+        <div>
+            <h1>Мой календарь<h1>
+        </div>
+        <!--Форма для добавления новой задачи-->
+        <div>
+            <h2>Новая задача</h2>
+        </div>
+        <div class="add-task">
+
+            <form>
+                <label for="theme">Тема:</label>
+                <input type="text" id="theme" name="theme" required><br>
+
+                <label for="type">Тип:</label>
+                <select id="type" name="type">
+                    <option value="meeting">Встреча</option>
+                    <option value="call">Звонок</option>
+                    <option value="meeting">Совещание</option>
+                    <option value="task">Дело</option>
+                </select><br>
+
+                <label for="location">Место:</label>
+                <input type="text" id="location" name="location"><br>
+
+                <label for="datetime">Дата и время:</label>
+                <input type="datetime-local" id="datetime" name="datetime" required><br>
+
+                <label for="duration">Длительность:</label>
+                <input type="text" id="duration" name="duration"><br>
+
+                <label for="comment">Комментарий:</label>
+                <textarea id="comment" name="comment"></textarea><br>
+
+                <input type="submit" value="Добавить задачу">
+            </form>
+        </div>
+        <div class="main-task">
+            <div>
+                <h2>Список задач</h2>
+            </div>
+            <div class="task-list">
+                <!-- Ссылки на различные списки задач -->
+                <select id="task-list">
+                    <option value="current-tasks">Текущие задачи</option>
+                    <option value="overdue-tasks">Просроченные задачи</option>
+                    <option value="completed-tasks">Выполненные задачи</option>
+                    <option value="tasks-on-date">Задачи на конкретную дату</option>
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <!-- <script>
+    // Обработчик изменения выбранного значения в списке
+    document.getElementById("task-list").addEventListener("change", function() {
+      var selectedOption = this.options[this.selectedIndex].value;
+      // Переход на выбранный список задач
+      window.location.href = selectedOption + ".html";
+    });
+  </script> -->
+</body>
+
+</html>
+</body>
+
+</html>
