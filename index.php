b0665df1b4a5d8b18b1783042b6684b9ea3f518e
Egor-Yarygin <108565142+Egor-Yarygin@users.noreply.github.com>
2023-05-25 22:55:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -92,50 +92,49 @@ require 'task_list.php';
                     <div>
                         <table>
                             <thead>
-                                <div class="table-elements"><tr>
-                                <div><th>Тип</th></div>
-                                <div>  <th>Тема</th></div>
-                                <div><th>Место</th></div>
-                                <div> <th>Дата и время</th></div>
-                                <div><th>Длительность</th></div>
-                                <div><th>Комментарий</th></div>
-                                </tr>
+                                <div class="table-elements">
+                                <div>Тип</div>
+                                <div>Тема</div>
+                                <div>Место</div>
+                                <div>Дата и время</div>
+                                <div>Длительность</div>
+                                <div>Комментарий</div>
 </div>
                             </thead>
                             <tbody id="task-list-body">
                                 <?php foreach ($tasks as $task): ?>
-                                    <tr class="table-elements">
+                                    <div class="table-elements">
                                         <div>
-                                        <td>
+                                        
                                             <?php echo $task['type']; ?>
-                                        </td>
+                                        
                                         </div>
                                         <div>
-                                        <td>
+                                        
                                             <?php echo $task['theme']; ?>
-                                        </td>
+                                        
                                         </div>
                                         <div>
-                                        <td>
+                                        
                                             <?php echo $task['location']; ?>
-                                        </td>
+                                        
                                         </div>
                                         <div>
-                                        <td>
+                                        
                                             <?php echo $task['datetime']; ?>
-                                        </td>
+                                        
                                         </div>
                                         <div>
-                                        <td>
+                                        
                                             <?php echo $task['duration']; ?>
-                                        </td>
+                                        
                                         </div>
                                         <div>
-                                        <td>
+                                        
                                             <?php echo $task['comment']; ?>
-                                        </td>
+                                        
                                 </div>
-                                    </tr>
+                                    
                                 <?php endforeach; ?>
                             </tbody>
                         </table>
